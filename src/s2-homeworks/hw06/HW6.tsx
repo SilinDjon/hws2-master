47185866a4cb0b73c99ec0d3d445bf50c7a51af1
SilinDjon <87773634+SilinDjon@users.noreply.github.com>
2023-02-14 20:48:09 +0300

    commit - hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,6 +19,7 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
+        restoreState<string>('hw6-editable-span-value', value)
         setValue(restoreState<string>('hw6-editable-span-value', value))
     }
 
