7bcf87d9a31b4c4f6dc68daf52d559866ec75abc
SilinDjon <87773634+SilinDjon@users.noreply.github.com>
2023-03-05 22:28:16 +0300

    commit - hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,14 +8,14 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             if(action.payload === 'up') {
-                return [...state.sort((a, b) => a.name < b.name ? -1 : 1)] // need to fix
+                return [...state].sort((a, b) => a.name < b.name ? -1 : 1) // need to fix
             }
-            return [...state.sort((a, b) => a.name > b.name ? -1 : 1)] // need to fix
+            return [...state].sort((a, b) => a.name > b.name ? -1 : 1) // need to fix
         }
 
         case 'check': {
 
-            return state.filter(el => el.age > action.payload) // need to fix
+            return state.filter(el => el.age >= action.payload) // need to fix
         }
         default:
             return state
