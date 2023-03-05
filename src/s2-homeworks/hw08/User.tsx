cafb196d057ed545c657fff026929227b62a99e3
SilinDjon <87773634+SilinDjon@users.noreply.github.com>
2023-03-05 21:49:27 +0300

    commit - hw8

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -12,10 +12,12 @@ const User: React.FC<UserPropsType> = ({ u }) => {
         <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
             <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
                 {/*отобразить имя*/}
+                {u.name}
 
             </td>
             <td id={'hw8-user-age-' + u._id}>
                 {/*отобразить возраст*/}
+                {u.age}
 
             </td>
         </tr>
